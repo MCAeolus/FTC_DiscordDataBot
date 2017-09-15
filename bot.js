69cad15f80506e5e855453c91b441427279d81a4
Nathan E. S <mcaeolusch@gmail.com>
2017-09-15 12:25:01 -0400

    update to first beta release.

--- bot.js
+++ bot.js
@@ -1,10 +1,12 @@
 const Discord = require('discord.js');
-const https = require('https');
+const http = require('http');
+const request = require('request');
 const fs = require('fs');
+const util = require('util');
 const client = new Discord.Client();
-const TOA_web = 'https://www.theorangealliance.org/api'
+const TOA_web = 'theorangealliance.org'
+const prefix = '+'
 var defaultsPackage = []
-const prefix = '!!'
 
 fs.readFile('defaults.txt', 'utf8', function(err,data) {
 	
@@ -15,47 +17,89 @@ fs.readFile('defaults.txt', 'utf8', function(err,data) {
 		
 		client.on('ready', () => {
 			console.log('Logged in as ' + client.user.tag + '.');
+			client.user.setGame('FTC - Team 4221');
 		});
 
 		client.on('message', msg => {
 			if(!msg.author.bot){
-				if(msg.content.toLowerCase().includes(prefix)){
-					var cmd = msg.content.substring(prefix.length,msg.content.length).toLowerCase();
-					if(cmd == "version"){
-							msg.reply(getTOA_Stats('').toString());
-					}else if (cmd == "test"){
+				if(msg.content.substring(0,prefix.length) == prefix && !msg.content.substring(prefix.length,prefix.length+1).includes(prefix.substring(0,1))){
+					var cmd = msg.content.substring(prefix.length,msg.content.length).split(' ');
+					cmd[0] = cmd[0].toLowerCase();
+					if(cmd[0] == "teamcount"){                                                    /* TEAM COUNT */
+						getTOA_Stats('/teams/count', function(e, r, d) {
+							j = JSON.parse(d);
+							msg.reply("There are " + j[0].TeamsCount + " teams registered for FTC.");
+						});
+					}else if(cmd[0] == "teaminfo"){                                               /* TEAM INFO */
+						if(cmd.length == 1)
+							msg.reply("Please pass the team number argument (" + prefix + "teamcount <team_number>)");
+						else{
+							getTOA_Stats('/team/'+cmd[1], function(e, r, d) {
+								j = JSON.parse(d);
+								if(j[0] == null)msg.reply("Team " + cmd[1] + " is not a registered team!");
+								else{
+									msg.reply("Team info for " + cmd[1]);
+									msg.channel.send("**Team Location** - " + j[0].city + ", " + j[0].region_key + ", " + j[0].country + "\n"
+											       + ((j[0].team_name_short != null) ? "**Team Name (Short)** - " + j[0].team_name_short + "\n" : "")
+											       + ((j[0].team_name_long != null) ? "**Team Name (Long)** - " + j[0].team_name_long + "\n" : "")
+											       + ((j[0].robot_name != null) ? "**Robot Name** - " + j[0].robot_name + "\n" : "")
+											       + "**Rookie Year** - " + j[0].rookie_year + "\n"
+											       + ((j[0].website != null) ? "**Website** - " + j[0].website : "")
+									);
+								}
+								
+							});
+						}
+					}else if(cmd[0] == "teamevents"){                                              /* TEAM EVENTS */
+						if(cmd.length == 1)
+							msg.reply("Please pass the team number argument (" + prefix + "teamevents <team_number>)");
+						else{
+							getTOA_Stats('/registration/team/'+cmd[1], function(e, r, d) {
+								j = JSON.parse(d);
+								if(j[0] == null)msg.reply("Team " + cmd[1] + " is either not a team or has no events registered.");
+								else{
+									msg.reply("Event registration info for team " + cmd[1]);
+									msg.channel.send(d);
+								}
+							});
+						}
+					}else if (cmd[0] == "date"){                                                   /* DATE */
 						var date = new Date();
 						msg.reply("Today's date: " + date.getDate() + "/"+ (date.getMonth()+1) + "/" + date.getFullYear() + ". The hour is " + date.getHours());	
-					}else if (cmd == "help"){
-						msg.reply("Real men don't need help. " + client.emojis.find("name", "HyperLul").toString());
+					}else if (cmd[0] == "help"){                                                   /* HELP */
+						mr = (Math.random() * 100) + 1;
+						if(mr > 90)
+							msg.reply("Real men don't need help. " + client.emojis.find("name", "HyperLul").toString());
+						else
+							msg.reply("**-- HELP --**\n"
+									 + prefix + "teamcount - returns total teams registered for FTC.\n"
+									 + prefix + "teaminfo <team_number> - returns the team info of a given FTC team.\n"
+									 + prefix + "teamevents <team_number> - returns all events the given FTC team is registered for."
+							);
 					}else{
-						msg.reply("That is not a valid command! Use !!help for a list of valid commmands.");
+						msg.reply("That is not a valid command! Use " + prefix + "help for a list of valid commmands.");
 					}
 				}
 			
-				if(msg.content.toLowerCase() == 'ping')msg.reply('pong');
+				if(msg.content.toLowerCase() == 'ping'){
+					msg.reply('pong');
+					msg.react(((Math.random()*100)+1 > 50) ? client.emojis.find("name","kcolon") : client.emojis.find("name","wherestheweed"));
+				}
 			}
 		});
 		client.login(defaultsPackage[0]);
 	}
 });
 
-function getTOA_Stats(apiSubRoutine){
-	var options = {	
-		host: TOA_web,
-		headers: { 'X-Application-Origin': 'Smida Syndicate', 'X-TOA-Key': defaultsPackage[1] }
-	}
-	const req = https.get(options
-	, (res) => {
-		res.on('data', (data) => {
-			console.log(data.toString());
-			return data;
-		});
-	});
-	req.on('error', (e)=>{
-		return 'error';
-	});
-	req.end();
+function getTOA_Stats(apiSubRoutine, callback){
 	
-	return 'no response';
+	var options = {
+		url: 'http://www.theorangealliance.org/api' + apiSubRoutine,
+		headers: {
+			'X-Application-Origin': 'Smida Syndicate',
+			'X-TOA-Key': defaultsPackage[1]
+		}
+	}
+	
+	request.get(options, callback);
 }
